145514af1154be3129750cb1293ed630e776911b
Zahra Paseban <108797287+ZahraP-4@users.noreply.github.com>
2025-12-30 17:47:03 -0800

    Add even or odd check program
    The program checks if a number is even or odd based on user input.

---
+++ problems/012_even_or_odd_check/solution.py
@@ -0,0 +1,10 @@
+#The program determines whether the given number is even or odd.
+
+#input/data
+num = int(input("Enter any number :"))
+
+#Calculations/Output
+if num % 2 == 0 :
+    print(num, "is even")
+else:
+    print(num, "is odd")
