de5189b112bf746ee395ebde0e075e1dae9debd8
Zahra Paseban <108797287+ZahraP-4@users.noreply.github.com>
2025-12-30 17:48:08 -0800

    Add vowel check program in solution.py
    The program checks if the entered character is a vowel and prints the result.

---
+++ problems/013_vowel_check/solution.py
@@ -0,0 +1,18 @@
+#The program determins whether the character entered is a vowel or not.
+
+#input/data
+char = input("Enter any character : ")
+
+#Calculations/Output
+if   char == "a" or char == "A":
+    print(char, "is a vowel")
+elif char == "e" or char == "E":
+    print(char, "is a vowel")
+elif char == "i" or char == "I":
+    print(char, "is a vowel")
+elif char == "o" or char == "O":
+    print(char, "is a vowel")
+elif char == "u" or char == "U":
+    print(char, "is a vowel")
+else:
+    print(char, "is not a vowel")
